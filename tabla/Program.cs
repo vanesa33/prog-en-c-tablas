0359ed9aa5ac365f28e2e5f433ebf0448a39f1c9
Vanidad <57409926+vanesa33@users.noreply.github.com>
2021-02-22 16:28:52 -0300

    TABLAS DE MULTIPLICAR

---
+++ tabla/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+namespace TABLAS_DE_MULTIPLICAR
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int NUM, RESUL, T, I;
+            string linea;
+            Console.Write("CUANTAS TABLAS: "); linea = Console.ReadLine();
+            NUM = int.Parse(linea);
+            for (T = 1; T <= NUM; T++)
+            {
+                for (I = 10; I >= 1; I--)
+                {
+                    RESUL = T * I;
+                }
+                Console.Write("Pulse una Tecla:"); Console.ReadLine();
+            }
+        }
+    }
+}
